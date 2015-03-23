61e78ae77a4349edeaede2bad77e2a4859e1e8a8
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-03-23 15:34:45 -0400

    Use host_inventory to get the ip address

--- test/integration/server/serverspec/default_spec.rb
+++ test/integration/server/serverspec/default_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'et_ntp::default' do
+describe 'et_ntp::server' do
   context 'installs /etc/ntp.conf' do
     describe file '/etc/ntp.conf' do
       its(:content) { is_expected.to match(/restrict/) }
@@ -10,7 +10,7 @@ describe 'et_ntp::default' do
   context 'should listen on port 123' do
     describe port 123 do
       it { is_expected.to be_listening.on('127.0.0.1').with('udp') }
-      it { is_expected.to be_listening.on(node['ipaddress']).with('udp') }
+      it { is_expected.to be_listening.on(host_inventory['ipaddress']).with('udp') }
     end
   end
 end
