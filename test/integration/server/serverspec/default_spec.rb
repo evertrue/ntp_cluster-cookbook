6602095691eeeeed80f6c954b42ace6574bdbdde
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-03-26 11:58:11 -0400

    Update Tests

--- test/integration/server/serverspec/default_spec.rb
+++ test/integration/server/serverspec/default_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe 'et_ntp::server' do
   context 'installs /etc/ntp.conf' do
     describe file '/etc/ntp.conf' do
-      its(:content) { is_expected.to match(/server 0[a-z0-9\.]+pool\.ntp\.org/) }
-      its(:content) { is_expected.to match(/server 1[a-z0-9\.]+pool\.ntp\.org/) }
-      its(:content) { is_expected.to match(/peer _default-ntp-1b\.priv\.evertrue\.com/) }
+      its(:content) { is_expected.to match(/server _default-ntp-1b\.priv\.evertrue\.com/) }
+      its(:content) { is_expected.to_not match(/server [^_]/) }
       its(:content) { is_expected.to match(/peer _default-ntp-1c\.priv\.evertrue\.com/) }
+      its(:content) { is_expected.to match(/peer _default-ntp-1d\.priv\.evertrue\.com/) }
     end
   end
 
