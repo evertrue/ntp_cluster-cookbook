31c3394ea0655b8bc00b01e6ec539ea8b3128319
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-03-26 17:17:55 -0400

    Update Tests

--- test/integration/server/serverspec/default_spec.rb
+++ test/integration/server/serverspec/default_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe 'et_ntp::server' do
   context 'installs /etc/ntp.conf' do
     describe file '/etc/ntp.conf' do
-      its(:content) { is_expected.to match(/server _default-ntp-1b\.priv\.evertrue\.com/) }
-      its(:content) { is_expected.to_not match(/server [^_]/) }
-      its(:content) { is_expected.to match(/peer _default-ntp-1c\.priv\.evertrue\.com/) }
-      its(:content) { is_expected.to match(/peer _default-ntp-1d\.priv\.evertrue\.com/) }
+      its(:content) { is_expected.to match(/^server _default-ntp-1b\.priv\.evertrue\.com/) }
+      its(:content) { is_expected.to_not match(/^server [^_]/) }
+      its(:content) { is_expected.to match(/^peer _default-ntp-1c\.priv\.evertrue\.com/) }
+      its(:content) { is_expected.to match(/^peer _default-ntp-1d\.priv\.evertrue\.com/) }
+      its(:content) { is_expected.to_not match(/^peer [^_]/) }
     end
   end
 
@@ -14,6 +15,14 @@ describe 'et_ntp::server' do
     describe port 123 do
       it { is_expected.to be_listening.on('127.0.0.1').with('udp') }
       it { is_expected.to be_listening.on(host_inventory['ipaddress']).with('udp') }
+    end
+  end
+
+  context 'should have the ntpcheck script' do
+    describe file '/usr/bin/ntpcheck' do
+      its(:content) { is_expected.to match(/\*_default-ntp-1b\.priv\.evertrue\.com/) }
+      its(:content) { is_expected.to match(/\(\\\+|\\\#\)_default-ntp-1c\.priv\.evertrue\.com/) }
+      its(:content) { is_expected.to match(/\(\\\+|\\\#\)_default-ntp-1d\.priv\.evertrue\.com/) }
     end
   end
 end
