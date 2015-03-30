0ec07254a46b70b1c6006f08c6a59dff8c1c8c0c
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-03-30 16:30:50 -0400

    Support anything greater than 12.04

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,7 @@ description      'Installs/Configures ntp_cluster'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-supports 'ubuntu', '~> 14.04'
-supports 'ubuntu', '~> 12.04'
+supports 'ubuntu', '>= 12.04'
 
 depends 'ntp', '~> 1.7'
 depends 'apt'
