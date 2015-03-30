f637f8775fb2e4f67d17728de195a92b2b477518
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-03-30 16:32:28 -0400

    Clean up the giant .each loop when resolving multimaster

--- recipes/discover.rb
+++ recipes/discover.rb
@@ -31,67 +31,46 @@ else
 end
 
 # Go through the pool and put the sandbys in the standbys list and masters in the masters list
-masters = pool.select do |n|
+master_nodes = pool.select do |n|
   n['tags'] && n['tags'].include?(node['ntp_cluster']['master_tag'])
 end
 
-masters = masters.map { |n| n['fqdn'] }.compact
+masters = master_nodes.map { |n| n['fqdn'] }.compact
 
 standbys = (pool.map { |n| n['fqdn'] } - masters).compact
 
 if masters.length > 1
+
+  masters_list =  masters.map { |fqdn| " * #{fqdn}" }.join "\n"
   log(
     "Chef found more than 1 ntp master in this cluster, this is not correct!\n" \
     "Only 1 node should be an ntp master for the cluster, otherwise there\n" \
-    "will be multiple true times.\n"
+    "will be multiple true times.\n" \
+    "Masters are:\n" \
+    "#{masters_list}"
   )
 
-  # Go through each of the nodes tagged as masters to see if we are an offending node and should
-  # become a standby node
-  masters.each do |master|
-    log(" * #{master} is listed as a master")
-    # In a multi-master situation, the master we look to will be the server with the highest fqdn
-    node.default['ntp_cluster']['master'] = node['fqdn'] if node['fqdn'] > node['ntp_cluster']['master']
-
-    # However, If one of the multiple masters is this node, then we will demote this node
-    next unless master == node['fqdn'] && node['fqdn']
-
-    log(
-      "  > #{node['fqdn']} is the node that this chef client is currently running on." \
-      "  > Demoting this node to a standby node\n"
-    )
-
-    # remove the ntp_master tag from this node
-    tags = node['tags']
-    tags.delete node['ntp_cluster']['master_tag']
-    node.normal['tags'] = tags.uniq
-    # Ensure that the tag was removed.  If not then fail.
-    if node['tags'].include? 'ntp_master'
-      fail '  > You are overriding me! Please check your overrides for the ntp_cluster/master/enabled attribute'
-    end
-
-    log "  > Chef has demoted #{node['fqdn']} to a standby node\n"
-
-    # Add this server to our list of standbys and remove it from our list of masters
+  if masters.include?(node['fqdn'])
     standbys << node['fqdn']
     masters.delete node['fqdn']
 
-    # If there is only 1 master remaining, then set that as the new master and break out of this loop
-    next unless masters.length == 1
+    # remove the master tage from this node
+    node.normal['tags'] = node['tags'].reject { |t| t == node['ntp_cluster']['master_tag'] }.uniq
 
-    node.default['ntp_cluster']['master'] = masters.first
-    standbys.delete masters.first
-    # We have a vaild master setup now do not continue
-    break
+    if node['tags'].include? 'ntp_master'
+      fail '  > You are overriding me! Please check your overrides for tags attribute'
+    end
   end
+
+  # by default in a multimaster env, the master is the node with the highest fqdn
+  node.default['ntp_cluster']['master'] = masters.max
+
 elsif masters.length == 1
   log 'Master is ' + masters.first
   node.default['ntp_cluster']['master'] = masters.first
 else
-  tags = node['tags']
-  tags = [] if tags.nil?
-  tags << 'ntp_master'
-  node.normal['tags'] = tags.uniq
+  tags = node['tags'] || []
+  node.normal['tags'] = tags.push(node['ntp_cluster']['master_tag']).uniq
 
   node.default['ntp_cluster']['master'] = node['fqdn']
 end
