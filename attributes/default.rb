28d884d8ab2e6c44d4b13c80a034f64310daa1b1
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-03-31 17:47:17 -0400

    Remove iburst declaration in server list as it is implied

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,10 +3,10 @@ default['ntp_cluster']['server_role'] = 'ntp_server'
 default['ntp_cluster']['pool'] = []
 
 default['ntp_cluster']['public_servers'] = [
-  '0.amazon.pool.ntp.org iburst',
-  '1.amazon.pool.ntp.org iburst',
-  '2.amazon.pool.ntp.org iburst',
-  '3.amazon.pool.ntp.org iburst'
+  '0.amazon.pool.ntp.org',
+  '1.amazon.pool.ntp.org',
+  '2.amazon.pool.ntp.org',
+  '3.amazon.pool.ntp.org'
 ]
 
 set['ntp']['servers'] = node['ntp_cluster']['public_servers']
