e1ef6b3fb51732b28afeb5fd1c824166da38b3f5
Jeff Byrnes <jeff@evertrue.com>
2015-09-02 14:49:12 -0400

    Drop the failure state from ntpcheck cronjob
    Cronitor checks do not have a “fail” mechanism

--- attributes/monitor.rb
+++ attributes/monitor.rb
@@ -2,7 +2,6 @@ default['ntp_cluster']['monitor']['enabled'] = false
 
 default['ntp_cluster']['monitor']['begin'] = 'true'
 default['ntp_cluster']['monitor']['success'] = 'true'
-default['ntp_cluster']['monitor']['fail'] = 'false'
 
 default['ntp_cluster']['monitor']['install_dir'] = '/usr/bin'
 
--- recipes/monitor.rb
+++ recipes/monitor.rb
@@ -27,9 +27,8 @@ cron_d 'ntpcheck' do
   hour node['ntp_cluster']['monitor']['hour']
   minute node['ntp_cluster']['monitor']['minute']
   command "bash -c '#{node['ntp_cluster']['monitor']['begin']}; " \
-          "( #{node['ntp_cluster']['monitor']['install_dir']}/ntpcheck " \
-          "&& #{node['ntp_cluster']['monitor']['success']} ) " \
-          "|| #{node['ntp_cluster']['monitor']['fail']}' " \
+          "#{node['ntp_cluster']['monitor']['install_dir']}/ntpcheck " \
+          "&& #{node['ntp_cluster']['monitor']['success']} " \
           '| logger -t ntpcheck -p cron.info'
   action node['ntp_cluster']['monitor']['enabled'] ? :create : :delete
 end
