1f71012f2bc4f63707a2b400f175b9e30ef5b0ea
Eric Herot <eric.github@herot.com>
2016-10-14 16:43:26 -0400

    minor array style tweak

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,12 +2,12 @@ default['ntp_cluster']['server_role'] = 'ntp_server'
 
 default['ntp_cluster']['pool'] = []
 
-default['ntp_cluster']['public_servers'] = [
-  '0.pool.ntp.org',
-  '1.pool.ntp.org',
-  '2.pool.ntp.org',
-  '3.pool.ntp.org'
-]
+default['ntp_cluster']['public_servers'] = %w(
+  0.pool.ntp.org
+  1.pool.ntp.org
+  2.pool.ntp.org
+  3.pool.ntp.org
+)
 
 default['ntp_cluster']['verify']['retries'] = 12
 default['ntp_cluster']['verify']['retry_delay'] = 5
